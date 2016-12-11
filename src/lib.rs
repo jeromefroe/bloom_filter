12ad1e622e517767325358eb366e463f78285cc3
Jerome Froelich <jeromefroe@uber.com>
2016-12-11 16:46:21 -0500

    Make Bloom generic over a single type

--- src/lib.rs
+++ src/lib.rs
@@ -20,6 +20,17 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
+//! An implementation of a bloom filter as as described in
+//! [Space/Time Trade-offs in Hash Coding with Allowable Errors] (http://dmod.eu/deca/ft_gateway.cfm.pdf).
+//!
+//! ## Example
+//!
+//! ``` rust,no_run
+//!
+//! fn main() {
+//! }
+//! ```
+
 extern crate bit_vec;
 extern crate rand;
 extern crate siphasher;
@@ -27,6 +38,7 @@ extern crate siphasher;
 use std::error::Error;
 use std::fmt;
 use std::hash::{Hash, Hasher};
+use std::marker::PhantomData;
 
 use bit_vec::BitVec;
 use siphasher::sip::SipHasher24;
@@ -91,7 +103,9 @@ impl BloomBuilder {
         self
     }
 
-    pub fn finish(&self) -> Result<Bloom, BloomError> {
+    pub fn finish<T>(&self) -> Result<Bloom<T>, BloomError>
+        where T: Hash
+    {
         match self.parameter {
             BloomParameter::Empty => Err(BloomError::NoParameterSet),
             BloomParameter::Size(size) => {
@@ -102,7 +116,7 @@ impl BloomBuilder {
             BloomParameter::FPR(p) => {
                 let min_size = BloomBuilder::min_size(self.elements, p);
                 let hash_count = BloomBuilder::optimal_hash_count(self.elements, min_size);
-                let bloom = Bloom::new(min_size, hash_count);
+                let bloom: Bloom<T> = Bloom::new(min_size, hash_count);
                 Ok(bloom)
             }
         }
@@ -125,7 +139,7 @@ impl BloomBuilder {
 }
 
 #[derive(Debug)]
-pub struct Bloom {
+pub struct Bloom<T: Hash> {
     bits: BitVec,
 
     // the number of hash functions to compute when inserting and looking up elements
@@ -134,15 +148,18 @@ pub struct Bloom {
     // we only need two independent hash functions because we can use double hashing to
     // create new independent hash functions from those two
     hashers: [SipHasher24; 2],
+
+    marker: PhantomData<T>,
 }
 
-impl Bloom {
+impl<T: Hash> Bloom<T> {
     fn new(size: u64, k: u32) -> Self {
 
         Bloom {
             bits: BitVec::from_elem(size as usize, false),
             k: k,
-            hashers: [Bloom::get_hasher(), Bloom::get_hasher()],
+            hashers: [Bloom::<T>::get_hasher(), Bloom::<T>::get_hasher()],
+            marker: PhantomData,
         }
     }
 
@@ -151,9 +168,7 @@ impl Bloom {
         SipHasher24::new_with_keys(rand::Rand::rand(&mut rng), rand::Rand::rand(&mut rng))
     }
 
-    pub fn insert<T>(&mut self, key: T)
-        where T: Hash
-    {
+    pub fn insert(&mut self, key: T) {
         // we need to save the results of the first two hashes for when we need to get more
         // than two hashes since we use double hashing uses those hashes to compute further hashes
         let mut hashes = [0u64, 0u64];
@@ -163,9 +178,7 @@ impl Bloom {
         }
     }
 
-    pub fn lookup<T>(&self, item: T) -> bool
-        where T: Hash
-    {
+    pub fn lookup(&self, item: T) -> bool {
         let mut hashes = [0u64, 0u64];
         for i in 0..self.k {
             let index = self.get_hash(&mut hashes, &item, i) % self.bits.len();
@@ -177,9 +190,7 @@ impl Bloom {
         true
     }
 
-    pub fn lookup_and_insert<T>(&mut self, key: T) -> bool
-        where T: Hash
-    {
+    pub fn lookup_and_insert(&mut self, key: T) -> bool {
         let mut hashes = [0u64, 0u64];
         let mut found = true;
         for i in 0..self.k {
@@ -193,9 +204,7 @@ impl Bloom {
         found
     }
 
-    fn get_hash<T>(&self, hashes: &mut [u64; 2], key: &T, i: u32) -> usize
-        where T: Hash
-    {
+    fn get_hash(&self, hashes: &mut [u64; 2], key: &T, i: u32) -> usize {
         if i < 2 {
             let hasher = &mut self.hashers[i as usize].clone();
             key.hash(hasher);
@@ -213,12 +222,12 @@ impl Bloom {
 
 #[cfg(test)]
 mod tests {
-    use super::{Bloom, BloomBuilder};
+    use super::BloomBuilder;
 
     #[test]
     fn test_with_size() {
         let size = 2u64.pow(20);
-        let b = BloomBuilder::new(size / 2).with_size(size).finish().unwrap();
+        let b = BloomBuilder::new(size / 2).with_size(size).finish::<i64>().unwrap();
         assert_eq!(b.bits.len() as u64, size);
         assert_eq!(b.k, 2);
     }
@@ -227,7 +236,7 @@ mod tests {
     fn test_with_fpr() {
         let elements = 2u64.pow(20);
         let fpr = 0.01;
-        let b = BloomBuilder::new(elements).with_fpr(fpr).finish().unwrap();
+        let b = BloomBuilder::new(elements).with_fpr(fpr).finish::<i64>().unwrap();
         println!("{:?}", b.bits.len());
         assert_eq!(b.k, 7);
     }
@@ -236,7 +245,7 @@ mod tests {
     fn test_operations() {
         let elements = 2u64.pow(20);
         let fpr = 0.01;
-        let mut b = BloomBuilder::new(elements).with_fpr(fpr).finish().unwrap();
+        let mut b = BloomBuilder::new(elements).with_fpr(fpr).finish::<i64>().unwrap();
 
         assert!(!b.lookup(3));
         assert!(!b.lookup(23));
